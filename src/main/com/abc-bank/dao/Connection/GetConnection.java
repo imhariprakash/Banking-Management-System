59d65de54f4e4293d55629bdb72107f5dc9b4031
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-10-13 17:19:48 +0530

    mysql getConnection method added

---
+++ src/main/com/abc-bank/dao/Connection/GetConnection.java
@@ -0,0 +1,17 @@
+package dao.Connection;
+
+public class GetConnection {
+    private GetConnection() {
+    } // Prevents instantiation
+
+    public static java.sql.Connection getConnection(String database) {
+        java.sql.Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, "root", "imhari");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return con;
+    }
+}
